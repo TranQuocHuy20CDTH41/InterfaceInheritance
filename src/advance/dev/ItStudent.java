8ea8d33ba17b9fafb783342fb3bb120b3ca2d86f
QuocHuy <huyhvker@gmail.com>
2021-06-14 22:37:15 +0700

    first commit

---
+++ src/advance/dev/ItStudent.java
@@ -0,0 +1,28 @@
+package advance.dev;
+public class ItStudent implements ICoder, IGamer, IHacker {
+
+	
+	@Override
+	public void hackFacebook() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void playGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String sleep(String time) {
+		// TODO Auto-generated method stub
+		return time;
+	}
+
+	@Override
+	public void code() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
